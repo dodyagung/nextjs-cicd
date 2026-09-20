9f2df2f05b8e19468108d9744bbbbaf2f591fc30
Dody <dody.saputro@jasaraharja.co.id>
2026-09-20 12:10:47 +0700

    chore: remove TODO comments and instant export for cache components adoption

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 import Image from "next/image";
 
-// TODO: Cache Components adoption. Refactor this route so this opt-out can be removed.
-// See: https://nextjs.org/docs/app/guides/migrating-to-cache-components
-export const instant = false;
-
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
